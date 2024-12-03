2bb73a157a3787537f3be7c43c6710212cd0a4e0
gpbsumido <gpbsumido@gmail.com>
2024-12-03 12:47:48 -0700

    add redeem codes features

---
+++ src/redeem_codes/index.ts
@@ -0,0 +1,47 @@
+import { Base } from "../base";
+import { RedeemCodesURL } from "../index";
+import _ from "lodash";
+
+export class REDEEM_CODES extends Base {
+  constructor(apiKey: string, baseUrl: RedeemCodesURL) {
+    super(apiKey, 'redeem_codes', false);
+
+    switch (baseUrl) {
+      case RedeemCodesURL.REDEEM_CODES_LOCAL: {
+        this.baseUrl = "https://api-stage.helika.io/v1";
+        this.enabled = false;
+        break;
+      }
+      case RedeemCodesURL.REDEEM_CODES_PROD: {
+        this.baseUrl = "https://api.helika.io/v1";
+        break;
+      }
+      case RedeemCodesURL.REDEEM_CODES_DEV:
+      default: {
+        this.baseUrl = "https://api-stage.helika.io/v1";
+        break;
+      }
+    }
+  }
+
+  //use a redeem code to get rewards
+  public async redeem(
+    redeem_code: string,
+    user_id: string
+  ): Promise<{ message: string }> {
+
+    let params: any = this.prepareEventParams(redeem_code, user_id)
+
+    return this.postRequest(`/redeem`, params);
+  }
+
+  private prepareEventParams(redeem_code: string, user_id: string) {
+
+    let params = {
+      redeem_code: redeem_code,
+      user_id: user_id
+    }
+    return params;
+  }
+
+}
